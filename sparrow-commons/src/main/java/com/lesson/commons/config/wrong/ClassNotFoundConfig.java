49584d5ea07e803cbff32094e64aa4785c9890b2
张立志 <zhanglizhi@tedu.cn>
2023-11-28 22:07:59 +0800

    not found case

--- sparrow-commons/src/main/java/com/lesson/commons/config/wrong/ClassNotFoundConfig.java
+++ sparrow-commons/src/main/java/com/lesson/commons/config/wrong/ClassNotFoundConfig.java
@@ -4,15 +4,25 @@ import com.alibaba.druid.pool.DruidDataSource;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
 @Configuration
+//@ConditionalOnClass(DruidDataSource.class)
 public class ClassNotFoundConfig {
+
     @Bean
-    public ExistingClass existingClass() {
-        return new ExistingClass();
-    }
-    @Bean(name = "sparrow_default")
     @ConditionalOnClass(DruidDataSource.class)
-    public DruidDataSource druidDataSource() {
+    public DruidDataSource existingClass() {
         return new DruidDataSource();
     }
+
+//    @Configuration(proxyBeanMethods = false)
+//    @ConditionalOnClass(DruidDataSource.class)
+//    static class DruidDataSource {
+//        @Bean(name = "sparrow_default")
+//        public DruidDataSource druidDataSource() {
+//            return new DruidDataSource();
+//        }
+//    }
 }
